a8a2b610407649e752973bb2a521dd50b991bb58
yavuz <yavuz.akpinar.68@gmail.com>
2022-02-17 23:25:37 +0300

    feat: Icons implemented

--- src/navigation/StackScreens.js
+++ src/navigation/StackScreens.js
@@ -2,15 +2,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import React from "react"
 import DetailPage from "../pages/detailPage/DetailPage"
 import MainPage from "../pages/mainPage/MainPage"
+import Icon from "react-native-vector-icons/Ionicons"
 
 const Stack = createNativeStackNavigator()
 
 const StackScreens = () => {
-  return(
-    <Stack.Navigator>
-    <Stack.Screen name="MainPage" component={MainPage}/>
-    <Stack.Screen name="DetailPage" component={DetailPage}/>
-  </Stack.Navigator>
+  return (
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="MainPage" component={MainPage} />
+      <Stack.Screen name="DetailPage" component={DetailPage} />
+    </Stack.Navigator>
   )
 }
 
--- src/navigation/TabStack.js
+++ src/navigation/TabStack.js
@@ -1,15 +1,29 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import React from "react"
+import Icon from "react-native-vector-icons/Ionicons"
 import ReservationPage from "../pages/reservationPage/ReservationPage"
 import StackScreens from "./StackScreens"
 
 const Tab = createBottomTabNavigator()
 
 const TabStack = () => {
-  return(
-    <Tab.Navigator>
-      <Tab.Screen name="StackScreens" component={StackScreens} />
-      <Tab.Screen name="ReservationPage" component={ReservationPage}/>
+  return (
+    <Tab.Navigator screenOptions={{
+      tabBarShowLabel: false, headerTitleAlign: "center"
+      , headerTitle: () => (
+        <Icon name="airplane" size={30} color="blue" />
+      )
+    }}>
+      <Tab.Screen options={{
+        tabBarIcon: () => (
+          <Icon name="home" size={20} color="red" />
+        )
+      }} name="StackScreens" component={StackScreens} />
+      <Tab.Screen options={{
+        tabBarIcon: () => (
+          <Icon name="pencil" size={20} color="red"/>
+        )
+      }} name="ReservationPage" component={ReservationPage} />
     </Tab.Navigator>
   )
 }
